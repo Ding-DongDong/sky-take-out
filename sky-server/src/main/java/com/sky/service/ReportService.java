bd5c54befe950861b5cc65aefdc70a92e6331b4a
dingdongdong <dingdongdong1217@gmail.com>
2024-08-27 22:45:01 +0800

    导出运营数据报表

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -5,9 +5,12 @@ import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 public interface ReportService {
+
+
 
     /**
      * 根据时间区间统计营业额
@@ -37,4 +40,9 @@ public interface ReportService {
      * @return
      */
     SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
+    /**
+     * 导出近30天的运营数据报表
+     * @param response
+     **/
+    void exportBusinessData(HttpServletResponse response);
 }
